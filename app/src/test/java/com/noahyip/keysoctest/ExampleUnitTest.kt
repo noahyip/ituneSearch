ce828ae54723e02e082ed64c8bb428c8e042ac85
Noah <noahyip0304@gmail.com>
2023-09-23 23:36:23 -0400

    [T-0001] Unit for MainActivityViewModel -Aim for full code coverage for MainActivityViewModel -mock api response for expected output

--- app/src/test/java/com/noahyip/keysoctest/ExampleUnitTest.kt
+++ app/src/test/java/com/noahyip/keysoctest/ExampleUnitTest.kt
@@ -1,17 +1,142 @@
-package com.noahyip.keysoctest
 
+import androidx.lifecycle.Observer
+import com.noahyip.keysoctest.apiService.ITuneService
+import com.noahyip.keysoctest.model.ITuneSearchResponse
+import com.noahyip.keysoctest.RetrofitClient
+import com.noahyip.keysoctest.viewModel.MainActivityViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
 
-import org.junit.Assert.*
+@ExperimentalCoroutinesApi
+class MainActivityViewModelTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var mockRetrofitClient: RetrofitClient
+
+    @Before
+    fun setUp() {
+        mockRetrofitClient = mock(RetrofitClient::class.java)
+        viewModel = MainActivityViewModel()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test search success`() {
+        // Arrange
+        val keyword = "test"
+        val expectedResponse = ITuneSearchResponse()
+        val call = mock(Call::class.java) as Call<ITuneSearchResponse>
+
+        `when`(mockRetrofitClient.getImgurService()).thenReturn(mock(ITuneService::class.java))
+        `when`(mockRetrofitClient.getImgurService().search(
+            "any",
+            20,
+            ITuneService.LIMIT,
+            "any",
+            "any",
+            "any"
+        )).thenReturn(call)
+
+        `when`(call.enqueue(any())).thenAnswer {
+            val callback = it.arguments[0] as Callback<ITuneSearchResponse>
+            callback.onResponse(call, Response.success(expectedResponse))
+        }
+
+        // Act
+        viewModel.search(keyword)
+
+        // Assert
+        assert(viewModel.iTuneSearchResponse.value == expectedResponse)
+    }
+
+    @Test
+    fun `test search failure`() {
+        // Arrange
+        val keyword = "test"
+        val call = mock(Call::class.java) as Call<ITuneSearchResponse>
+
+        `when`(mockRetrofitClient.getImgurService()).thenReturn(mock(ITuneService::class.java))
+        `when`(mockRetrofitClient.getImgurService().search(
+            "any",
+            20,
+            ITuneService.LIMIT,
+            "any",
+            "any",
+            "any"
+        )).thenReturn(call)
+
+        `when`(call.enqueue(any())).thenAnswer {
+            val callback = it.arguments[0] as Callback<ITuneSearchResponse>
+            callback.onFailure(call, IOException("Test Exception"))
+        }
+        // Act
+        viewModel.search(keyword)
+
+        // Assert
+        assert(viewModel.iTuneSearchResponse.value == null)
+    }
+
+    @Test
+    fun `test loadMore success`() {
+        // Arrange
+        val moreResponse = ITuneSearchResponse()
+        val call = mock(Call::class.java) as Call<ITuneSearchResponse>
+
+        `when`(mockRetrofitClient.getImgurService()).thenReturn(mock(ITuneService::class.java))
+        `when`(mockRetrofitClient.getImgurService().search(
+            "any",
+            20,
+            ITuneService.LIMIT,
+            "any",
+            "any",
+            "any"
+        )).thenReturn(call)
+
+        `when`(call.enqueue(any())).thenAnswer {
+            val callback = it.arguments[0] as Callback<ITuneSearchResponse>
+            callback.onResponse(call, Response.success(moreResponse))
+        }
+
+        // Act
+        viewModel.loadMore()
+        assertTrue(viewModel.iTuneSearchResponse.value?.hasMore ?: true)
+    }
+
+    @Test
+    fun `test loadMore failure`() {
+        // Arrange
+        val call = mock(Call::class.java) as Call<ITuneSearchResponse>
+
+        `when`(mockRetrofitClient.getImgurService()).thenReturn(mock(ITuneService::class.java))
+        `when`(mockRetrofitClient.getImgurService().search(
+            "any",
+            20,
+            ITuneService.LIMIT,
+            "any",
+            "any",
+            "any"
+        )).thenReturn(call)
+
+        `when`(call.enqueue(any())).thenAnswer {
+            val callback = it.arguments[0] as Callback<ITuneSearchResponse>
+            callback.onFailure(call, IOException("Test Exception"))
+        }
+
+        // Act
+        viewModel.loadMore()
+        assertFalse(viewModel.iTuneSearchResponse.value?.hasMore ?: false)
     }
 }
